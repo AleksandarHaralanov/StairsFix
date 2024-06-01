5365a86f666deda76c63021374d3ad3e39dccb70
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-06-02 01:36:34 +0300

    Small fix

--- src/main/java/me/beezle/stairsfix/BlockHandler.java
+++ src/main/java/me/beezle/stairsfix/BlockHandler.java
@@ -8,18 +8,15 @@ import org.bukkit.inventory.ItemStack;
 
 public class BlockHandler implements Listener {
     @EventHandler
-    public void onStairsBreak(final BlockBreakEvent event)
-    {
-        switch (event.getBlock().getType())
-        {
+    public void onStairsBreak(final BlockBreakEvent event) {
+        switch (event.getBlock().getType()) {
             case WOOD_STAIRS:
                 event.setCancelled(true);
                 event.getBlock().getWorld().getBlockAt(event.getBlock().getLocation()).setType(Material.AIR);
                 event.getPlayer().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.WOOD_STAIRS, 1));
                 break;
             case COBBLESTONE_STAIRS:
-                if (isType(event.getPlayer().getItemInHand(), Material.WOOD_PICKAXE, Material.STONE_PICKAXE, Material.IRON_PICKAXE, Material.DIAMOND_PICKAXE, Material.GOLD_PICKAXE))
-                {
+                if (isType(event.getPlayer().getItemInHand(), Material.WOOD_PICKAXE, Material.STONE_PICKAXE, Material.IRON_PICKAXE, Material.DIAMOND_PICKAXE, Material.GOLD_PICKAXE)) {
                     event.setCancelled(true);
                     event.getBlock().getWorld().getBlockAt(event.getBlock().getLocation()).setType(Material.AIR);
                     event.getPlayer().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.COBBLESTONE_STAIRS, 1));
@@ -30,12 +27,9 @@ public class BlockHandler implements Listener {
         }
     }
 
-    public boolean isType(ItemStack itemStack, Material... materials)
-    {
-        for (Material mat : materials)
-        {
-            if (itemStack.getType() == mat)
-            {
+    public boolean isType(ItemStack itemStack, Material... materials) {
+        for (Material mat : materials) {
+            if (itemStack.getType() == mat) {
                 return true;
             }
         }
--- src/main/java/me/beezle/stairsfix/StairsFix.java
+++ src/main/java/me/beezle/stairsfix/StairsFix.java
@@ -3,16 +3,13 @@ package me.beezle.stairsfix;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class StairsFix extends JavaPlugin
-{
-    public void onEnable()
-    {
+public class StairsFix extends JavaPlugin {
+    public void onEnable() {
         Bukkit.getServer().getPluginManager().registerEvents(new BlockHandler(), this);
         System.out.print("[StairsFix] Enabled.");
     }
 
-    public void onDisable()
-    {
+    public void onDisable() {
         System.out.print("[StairsFix] Disabled.");
     }
 }
