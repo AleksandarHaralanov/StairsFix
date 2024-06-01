a21577fb68956be32ee42463d2fab32063565bed
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-06-02 02:16:41 +0300

    Remove import

--- src/main/java/me/beezle/stairsfix/StairsFix.java
+++ src/main/java/me/beezle/stairsfix/StairsFix.java
@@ -1,11 +1,10 @@
 package me.beezle.stairsfix;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StairsFix extends JavaPlugin {
     public void onEnable() {
-        Bukkit.getServer().getPluginManager().registerEvents(new BlockHandler(), this);
+        this.getServer().getPluginManager().registerEvents(new BlockHandler(), this);
         System.out.print("[StairsFix] Enabled.");
     }
 
