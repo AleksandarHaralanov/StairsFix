f3a3cdc8e6b7fd0b615422270975b16ee09da80f
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2025-01-08 07:01:03 +0200

    Simplify code

--- src/main/java/io/github/aleksandarharalanov/stairfix/listener/BlockBreakListener.java
+++ src/main/java/io/github/aleksandarharalanov/stairfix/listener/BlockBreakListener.java
@@ -1,7 +1,6 @@
 package io.github.aleksandarharalanov.stairfix.listener;
 
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -17,7 +16,7 @@ public class BlockBreakListener extends BlockListener {
         Block block = event.getBlock();
         Player player = event.getPlayer();
 
-        if (block.getType() != Material.COBBLESTONE_STAIRS && block.getType() != Material.WOOD_STAIRS) return;
+        if (block.getType() != Material.WOOD_STAIRS && block.getType() != Material.COBBLESTONE_STAIRS) return;
         if (block.getData() > (byte) 3) return;
 
         if (block.getType() == Material.COBBLESTONE_STAIRS) {
@@ -25,20 +24,12 @@ public class BlockBreakListener extends BlockListener {
             if (itemInHand == null || !itemInHand.getType().toString().contains("PICKAXE")) return;
         }
 
+        WorldGuardPlugin worldGuard = (WorldGuardPlugin) getServer().getPluginManager().getPlugin("WorldGuard");
+        if (worldGuard != null && worldGuard.isEnabled())
+            if (!worldGuard.canBuild(player, block.getLocation())) return;
+
         event.setCancelled(true);
         Material correctDrop = block.getType() == Material.WOOD_STAIRS ? Material.WOOD_STAIRS : Material.COBBLESTONE_STAIRS;
-
-        // Check if WorldGuard is present and player can build in the region the stairs block is located
-        WorldGuardPlugin worldGuard = (WorldGuardPlugin) getServer().getPluginManager().getPlugin("WorldGuard");
-        if (worldGuard != null && worldGuard.isEnabled()) {
-            if (worldGuard.canBuild(player, block.getLocation())) {
-                block.setType(Material.AIR);
-                block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(correctDrop, 1));
-            } else player.sendMessage(ChatColor.DARK_RED + "You don't have permission for this area.");
-            return;
-        }
-
-        // If WorldGuard is not present, process normally
         block.setType(Material.AIR);
         block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(correctDrop, 1));
     }
